092c0639758899944c00510d213dc4ce8e0c7746
madhuri <guravmadhuri93@gmail.com>
2025-02-09 22:15:23 +0530

    msg qa

--- FirstPracticeDemo/src/com/bit/demo1/HelloWorld.java
+++ FirstPracticeDemo/src/com/bit/demo1/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+System.out.println("qa");
+System.out.println("add");
+System.out.println("Hello world");
+System.out.println("code");
+
 
 	}
 
