7132af90071ec251e3f47defc2844447b579733a
madhuri <guravmadhuri93@gmail.com>
2025-02-10 23:37:23 +0530

    msg from qa

--- FirstPracticeDemo/src/com/bit/demo1/HelloWorld.java
+++ FirstPracticeDemo/src/com/bit/demo1/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello world");
 System.out.println("qa");
 System.out.println("add");
+System.out.println("Hello ");
+System.out.println("Hello world5578");
 System.out.println("Hello world");
 System.out.println("code");
 
